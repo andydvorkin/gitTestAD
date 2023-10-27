d199522dca5a3de881a22f7fa28f5e649b97499a
Andy <andydvorkin@gmail.com>
2023-10-27 10:05:32 +0200

    another test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Das ist mein Test!!!");
+        System.out.println("Das ist ein weiterer Test");
     }
 }
